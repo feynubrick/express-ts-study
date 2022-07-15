71b44e5114de0bb82ec070fc821719c3939ae462
Seung-Yoon Oh <feynubrick@gmail.com>
2022-07-16 00:20:21 +0900

    change logger message format

--- src/middlewares/request-logger.ts
+++ src/middlewares/request-logger.ts
@@ -5,7 +5,8 @@ export default function(req: Request, res: Response, next: NextFunction) {
     const currentTime = module.time.printAsIso(new Date());
     const method = req.method;
     const path = req.path;
+    const query = JSON.stringify(req.query);
     const body = JSON.stringify(req.body);
-    console.log(`[${currentTime}] ${method} ${path} : ${body}`);
+    console.log(`[${currentTime}] ${method} ${path} || query: ${query} || body: ${body}`);
     next();
 };
