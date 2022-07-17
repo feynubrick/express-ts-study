e5c9a57f9ab7dc5c49dbc2fd2daa53f3291c9b1f
Seung-Yoon Oh <feynubrick@gmail.com>
2022-07-17 22:20:17 +0900

    edit eslintrc to use default setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,13 +13,5 @@ module.exports = {
     browser: true,
     commonjs: true,
   },
-  rules: {
-    "no-undef": "warn",
-    "no-console": "warn", // console 전역변수의 사용에 경고
-    "valid-typeof": "error", // typeof 비교에 올바른 문자열 사용 못하면 에러
-    eqeqeq: "error", // 비교 연산에 ==, !=을 사용하면 에러
-    "no-unused-vars": "warn", // 사용되지 않은 변수가 있으면 에러
-    "no-shadow": "error", // 하위 스코프에서 상위 스코프의 변수명 쓰면 에러
-    "no-var": "error", // var 사용하면 에러
-  },
+  rules: {},
 };
